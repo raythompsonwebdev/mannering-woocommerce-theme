9243691b3b80a95b424299de5cbdae647a3a64b8
raythompsonwebdev <ray_thomp@hushmail.com>
2022-12-22 23:36:48 +0000

    added postcss - wp-scripts lint not working

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
 module.exports = {
+	// eslint-disable-next-line global-require
+	syntax: require('postcss-scss'),
 	plugins: [
 		// eslint-disable-next-line global-require
-		require("@csstools/postcss-sass")(/* node-sass options */),
+		require('@csstools/postcss-sass')(/* node-sass options */),
 		// eslint-disable-next-line global-require
-		require("autoprefixer")(),
+		require('autoprefixer')(),
 		// eslint-disable-next-line global-require
-		require("postcss-preset-env")({
+		require('postcss-preset-env')({
 			stage: 1,
 		}),
-
+		// eslint-disable-next-line global-require
+		require('stylelint'),
 		// eslint-disable-next-line global-require
 		// require('cssnano')({
 		// preset: 'default',
